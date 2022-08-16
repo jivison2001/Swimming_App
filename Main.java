e7e3d4fffd2c2dc6240270bd5fb6cee2ddab9d8b
jivison2001 <ivisonj@lancaster.ac.uk>
2022-08-16 13:47:26 +0100

    Added buttons to main menu screen, created class for constant

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    
+    MainScreen m = new MainScreen();
   }
 }
--- MainScreen.java
+++ MainScreen.java
@@ -1,3 +1,38 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
 public class MainScreen {
-  
+  private JFrame frame;
+  private JPanel mainPanel;
+  private JButton recordTime;
+  private JButton viewIndTimes;
+  private JButton viewEventTimes;
+  private JButton viewTeamSheet;
+  public MainScreen() {
+    frame = new JFrame();
+    mainPanel = new JPanel(new GridLayout(4,1));
+    mainPanel.setBounds(50,50,475,300);
+    recordTime = new JButton("Record Time");
+    viewIndTimes = new JButton("View Indivudal Times");
+    viewEventTimes = new JButton("View Times By Event");
+    viewTeamSheet = new JButton("Generate Team Sheet");
+    
+    frame_settings();
+
+    mainPanel.add(recordTime);
+    mainPanel.add(viewIndTimes);
+    mainPanel.add(viewEventTimes);
+    mainPanel.add(viewTeamSheet);
+  }
+
+  private void frame_settings() {
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+    frame.setResizable(false);
+    frame.setVisible(true);
+    frame.setLayout(null);
+    frame.add(mainPanel);
+  }
 }
