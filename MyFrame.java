2fe56627b9c8754c4b0275109792a87d3a06f063
jivison2001 <ivisonj@lancaster.ac.uk>
2022-08-16 14:00:50 +0100

    Created a frame class that extends jframe to reduce amount of code in mainscreen

---
+++ MyFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+  public void frame_settings() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+    this.setResizable(false);
+    this.setVisible(true);
+    this.setLayout(null);
+  }
+}
